7460e962841ed62110d52241d7a74dc4a978276a
MarwanMaher0 <Marwanmaher635@gmail.com>
2024-06-29 08:16:13 +0300

    vue home done

--- src/components/EquipmentSaidBar.vue
+++ src/components/EquipmentSaidBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-1/4 bg-gray-700 mt-16 p-1">
+    <div class="w-1/4 bg-gray-700 mt-2 p-1">
         <ul class="flex flex-col space-y-4 text-lg font-bold text-white">
             <li>
                 <router-link class="custom-link" to="/AddEquipment">Add Equipment</router-link>
