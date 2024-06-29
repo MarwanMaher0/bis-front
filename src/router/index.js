12053c12f3a77bf0ff1c5082392fb661085446d3
MarwanMaher0 <Marwanmaher635@gmail.com>
2024-06-29 07:53:42 +0300

    vue home done

--- src/router/index.js
+++ src/router/index.js
@@ -19,8 +19,8 @@ const router = createRouter({
       component: () => import("../views/AllEquipment.vue"),
     },
     {
-      path: "/description",
-      name: "description",
+      path: "/Description",
+      name: "Description",
       component: () => import("../views/Description.vue"),
     },
     {
