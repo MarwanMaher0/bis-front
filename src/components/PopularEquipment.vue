72cd63477e0082aa779cea4645035f43a8fea950
MarwanMaher0 <Marwanmaher635@gmail.com>
2024-07-11 21:04:21 +0300

    asdz

--- src/components/PopularEquipment.vue
+++ src/components/PopularEquipment.vue
@@ -4,15 +4,18 @@
       <h1 class="text-white text-3xl text-center mb-8">Popular Equipment</h1>
       <div class="grid grid-cols-3 gap-5">
         <div class="h-[300px]" v-for="(item, index) in equipment" :key="index">
-          <div class="relative h-[300px]  rounded-lg transform transition-transform duration-500 hover:scale-105">
-            <img :src="item.imageUrl" alt="Equipment" class="h-full w-full">
-            <div
-              class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-4 text-white flex flex-col justify-end">
-              <p class="text-sm"><i class="fa-regular fa-compass"></i> {{ item.machineName }}</p>
-              <h4 class="text-xl font-semibold">{{ item.description }}</h4>
-              <p class="text-lg">From ${{ item.priceOfRent }} per day</p>
+          <router-link :to="{ name: 'Description', params: { id: item.id } }">
+
+            <div class="relative h-[300px]  rounded-lg transform transition-transform duration-500 hover:scale-105">
+              <img :src="item.imageUrl" alt="Equipment" class="h-full w-full">
+              <div
+                class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-4 text-white flex flex-col justify-end">
+                <p class="text-sm"><i class="fa-regular fa-compass"></i> {{ item.machineName }}</p>
+                <h4 class="text-xl font-semibold">{{ item.description }}</h4>
+                <p class="text-lg">From ${{ item.priceOfRent }} per day</p>
+              </div>
             </div>
-          </div>
+          </router-link>
         </div>
       </div>
       <router-link to="/AllEquipment">
